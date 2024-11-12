6d21b8ece0847e9b189a969d55bd6a7111c419b9
mmm75321 <michel@one8.ch>
2024-11-12 09:59:31 +0100

    Update amtsblatt.py
    Mehrere Suchbegriffe
    Kommentare übersetzt

--- amtsblatt.py
+++ amtsblatt.py
@@ -1,54 +1,66 @@
-# import packages
+# Pakete importieren
 import pypdf
 import re
 import http.client
 import urllib
 import wget
 import datetime
+import os
 
-#pushover variables
+# Pushover Variablen: User und Token eintragen
 po_user = "***"
 po_token = "***"
 
-#search for string, ideally postal code
+# Suchbegriff eintragen, idealerweise Postleitzahl
+# Mehrere Begriffe mit Komma trennen
+# Keine Leerschläge
 search = "3280"
 
-#end of sections which needs to be modified
-#other variables
+# Änderungen unterhalb von hier nicht notwendig
+# Weitere Variablen (zur Laufzeit gesetzt
 date = datetime.datetime.now() + datetime.timedelta(days=-4)
 url = "https://abl.fr.ch/system/files/issues/pdfs/FO_" + date.strftime("%G") + "-" + date.strftime("%W") + ".pdf"
 title = f"Amtsblatt Ausgabe " + date.strftime("%W") + " - " + date.strftime("%G")
-message = f"{url}\n"
+message = f"Suche nach {search} in \n{url}\n\n"
 send = False
 
+# Funktion um Pushovernachricht zu senden
 def send_pushover(title, message):
     conn = http.client.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json", urllib.parse.urlencode({"token": po_token,"user": po_user,"message": message,"title": title})
         , { "Content-type": "application/x-www-form-urlencoded" })
     conn.getresponse()
 
+# PDF Datei herunterladen
 try:
     pdffile = wget.download(url)
+    print("")
 except:
     send_pushover(title, f"Download von {url} fehlgeschlagen.")
 
-# open the pdf file
+# PDF Datei öffnen
 reader = pypdf.PdfReader(pdffile)
 
-# get number of pages
+# Anzahl Seiten der PDF Datei ermitteln
 num_pages = len(reader.pages)
 
-# extract text and do the search
-for index, page in enumerate(reader.pages):
-    text = page.extract_text()
-    res_search = re.search(search, text)
-    if (match := res_search) is not None:
-        message = f"{message} Eintrag gefunden auf Seite {index+1}\n"
-        send = True
+# Suchbegriff nach Kommas trennen
+splitsearch = [split.strip() for split in search.split(',')]
 
+# Suche durchführen (Seite für Seite, Suchbegriff für Suchbegriff)
+for pindex, page in enumerate(reader.pages):
+    text = page.extract_text()
+    for svalue in splitsearch:
+        res_search = re.search(svalue, text)
+        if (match := res_search) is not None:
+            message = f"{message}Eintrag {svalue} gefunden auf Seite {pindex+1}\n"
+            send = True
+
+# PDF Datei löschen
 os.remove(pdffile)
 
+# Pushover Nachricht senden
 if send:
     send_pushover(title, message)
 else:
-    send_pushover(title,f"Wert {search} in {url} nicht gefunden.")
+    send_pushover(title,f"{message}Keine Einträge gefunden.")
